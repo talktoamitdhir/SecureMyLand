6296bb6dfe2e6b7360d40adfa7fb5a27ebf3a5d4
Amit Dhir <talktoamitdhir@gmail.com>
2024-03-04 09:37:56 -0800

    Added the React Components for both Search and Addition of an appointment to the App

--- frontinterface/src/App.js
+++ frontinterface/src/App.js
@@ -1,12 +1,16 @@
 import {BiArchive} from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointments from "./components/AddAppointments";
 
 function App() {
   return (
-    <div className="App container mx-auto mt-1 font-thin">            
+    <div className="App container mx-auto mt-1 font-thin p-10">            
       <h1 className="text-5xl">
         <BiArchive className="inline-block text-red-400 align-top" />
         Your Appointments are ready
-      </h1>      
+      </h1>
+      <Search/>     
+      <AddAppointments /> 
     </div>
   );
 }
