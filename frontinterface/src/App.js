1e59095ca35e3e04839b0db8c2a5d5116c9b38c7
Amit Dhir <talktoamitdhir@gmail.com>
2024-03-04 10:51:56 -0800

    Added the Child Component along with the Destruction, Arrow Function and Use State Understanding

--- frontinterface/src/App.js
+++ frontinterface/src/App.js
@@ -1,16 +1,26 @@
-import {BiArchive} from "react-icons/bi";
+import {BiCalendar} from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointments from "./components/AddAppointments";
+import appointmentList from "./data.json";
+import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
   return (
     <div className="App container mx-auto mt-1 font-thin p-10">            
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top" />
-        Your Appointments are ready
+      <h1 className="text-5xl text-center">
+        <BiCalendar className="inline-block align-top text-blue-400" />
+        Your Appointments
       </h1>
-      <Search/>     
-      <AddAppointments /> 
+      <AddAppointments/> 
+      <Search/>  
+      <ul className="divide-y divide-gray-200 py-5">        
+        {appointmentList.map(eachAppointment=>(
+          <AppointmentInfo 
+          key={eachAppointment.id} 
+          eachAppointment={eachAppointment} />
+        ))
+        }
+      </ul>
     </div>
   );
 }
