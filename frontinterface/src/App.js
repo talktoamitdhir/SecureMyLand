3135d22393ab8ddd186d5e27761bab0aba350afd
Amit Dhir <talktoamitdhir@gmail.com>
2024-03-04 13:45:39 -0800

    used useCallback, useEffect and useState to Fetch the data from API

--- frontinterface/src/App.js
+++ frontinterface/src/App.js
@@ -1,10 +1,20 @@
+import { useState,useEffect,useCallback } from "react";
 import {BiCalendar} from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointments from "./components/AddAppointments";
-import appointmentList from "./data.json";
 import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+  
+  let fetchData = useCallback(()=>{
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(jsonData => setAppointmentList(jsonData));
+  },[]);
+
+  useEffect(() => { fetchData(); },[fetchData]);
+
   return (
     <div className="App container mx-auto mt-1 font-thin p-10">            
       <h1 className="text-5xl text-center">
